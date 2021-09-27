e73fd1de2171af4c26bb0c0750593fae14c3c375
Puneet Dadhich <puneetdadhich19@gmail.com>
2021-09-27 16:14:35 +0530

    Create code1.cpp

---
+++ Basic Datatypes/code1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    
+ int p;
+ long long int q;
+ char r;
+ float s;
+ double t;
+
+ scanf("%d %lld %c %f %lf",&p,&q,&r,&s,&t);
+
+ printf("%d\n%lld\n%c\n%f\n%lf",p,q,r,s,t);
+
+return 0;
+}
