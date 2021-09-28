cecbbb6952341e0b791d784495dd26b4c58ffdbd
Puneet Dadhich <puneetdadhich19@gmail.com>
2021-09-28 12:44:59 +0530

    Create code2.cpp

---
+++ input and output/code2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    int amount1;
+    cin>>amount1;
+
+    int amount2;
+    cin>>amount2;
+
+    int amount3;
+    cin>>amount3;
+
+    int sum=amount1+amount2+amount3;
+    cout<<sum<<endl;
+
+    return 0;
+
+    
+}
