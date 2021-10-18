1ca4e95914bcf2ec2c2dd8356d7a373ca61b21d7
Puneet Dadhich <90840992+Puneet019@users.noreply.github.com>
2021-10-18 12:09:01 +0530

    code1

--- Basic Datatypes/code1.cpp
+++ Basic Datatypes/code1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){ 
+int main(){                             \\To read a datatypes ,use the syntax : scanf("`format_specifier`", &val)
 
  int p;
  long long int q;
